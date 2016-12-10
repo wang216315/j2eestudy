b04b3ca36e208d5cbe52074f4d2712acef162357
wangyue <wangyue@wangyue01>
2016-12-10 23:45:40 +0800

    OK

--- WebContent/WEB-INF/content/loginform.jsp
+++ WebContent/WEB-INF/content/loginform.jsp
@@ -1,12 +1,17 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+ <%@ taglib prefix="s" uri="/struts-tags"  %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>登陆页面</title>
+<title>Insert title here</title>
 </head>
 <body>
-的发给房东打工
+<s:form action="login">
+	<s:textfield name="username" label="Username" />
+	<s:password name="password" label="Password" />
+	<s:submit value="Login" />
+</s:form>
 </body>
 </html>
--- src/com/will/app/action/LoginAction.java
+++ src/com/will/app/action/LoginAction.java
@@ -4,11 +4,35 @@ import com.opensymphony.xwork2.Action;
 
 public class LoginAction implements Action
 {
-
+	private String username;
+	private String password;
+	
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	public String getUsername()
+	{
+		return this.username;
+	}
+	
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+	
+	public String getPassword()
+	{
+		return this.password;
+	}
+	
 	@Override
 	public String execute() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if(this.username.equals("wangyue")&& this.password.equals("123"))
+		{
+			return SUCCESS;
+		}
+		return ERROR;
 	}
 	
 }
